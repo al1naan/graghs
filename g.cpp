9b783d2cc6c102e4fa4727e7d022311df3506d59
Alina <alina.parashchiy@gmail.com>
2023-08-12 11:48:13 +0300

    Create g.cpp

---
+++ g.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+//	freopen("art.in", "r", stdin);
+//	freopen("art.out", "w", stdout);
+
+	pair<int, vector<vector<int>>> neigh;
+
+	int ver, reb, x, y, dov, ans;
+	cin >> ver >> reb;
+	
+	
+
+}
